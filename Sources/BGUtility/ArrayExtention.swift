e7bacb9abb6466ce7be84e13c02953721865cc45
LovaRK <plovark@gmail.com>
2024-12-08 11:44:14 +0530

    ArrayExtention added.

--- Sources/BGUtility/ArrayExtention.swift
+++ Sources/BGUtility/ArrayExtention.swift
@@ -1,12 +1,12 @@
 //
-//  Untitled.swift
+//  ArrayExtention.swift
 //  BGUtility
 //
 //  Created by Saanvika on 08/12/24.
 //
 
 public extension Array {
-    subscript (safe index: Int) -> Element? {
+    subscript (safeIndex index: Int) -> Element? {
         return indices ~= index ? self[index] : nil
     }
 }
